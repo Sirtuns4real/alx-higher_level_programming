f5fa293b12f8d105cc2a71ff0a81e50e7ee91845
sirtuns4real <olatundoloyede41@yahoo.com>
2023-01-02 00:57:20 -0800

    task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - function that checks for if a singlt list has a cycle
+ *
+ * @listint_t: first argument
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int check_cycle(listint_t *list)
+
+{
+
