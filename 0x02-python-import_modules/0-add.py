fbb6a7af8216ec132d070c4b111780cb01c12e8d
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-01-05 12:20:55 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import math
+from add_0 import add
 def add(a, b):
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, sum(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
