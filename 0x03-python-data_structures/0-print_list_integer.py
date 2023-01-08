3c44484c2e8a854c32b4270734289c847bb77e37
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-01-08 18:35:58 +0100

    Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
   for list_element in range(len(my_list)):
-    print("{:d}" .format(my_list[list_element]))
+    print("{:d}".format(my_list[list_element]))
