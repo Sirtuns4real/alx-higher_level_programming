b13239cdddacfff9fab34d0b1452130a2ac2e37a
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-04-27 00:53:54 +0100

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-const sizer = parseInt(process.argv[2]);
-if (Number.isNaN(sizer)) {
+const sizer = Math.floor(Number(process.argv[2]));
+if (isNaN(sizer)) {
   console.log('Missing size');
 } else {
-  for (let i = 0, s; i < sizer; i++) {
-    s = '';
-    for (let j = 0; j < sizer; j++) {
-      s += 'X';
-    }
-    console.log(s);
+  for (let r = 0; r < sizer; r++) {
+    let rw = '';
+    for (let c = 0; c < sizer; c++) rw += '*';
+    console.log(rw);
   }
+}
