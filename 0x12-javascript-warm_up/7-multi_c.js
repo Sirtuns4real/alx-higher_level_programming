4d1796d707653e3f30674966df97edf901590ad2
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-04-27 00:45:43 +0100

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const newNum = Math.floor(Number(process.argv[2]));
-if (isNaN(newNum)) {
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < x; i++) {
