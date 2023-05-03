0c0dce4d697e5225d06bc2263d024d966eae4be6
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-05-03 15:58:42 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines a peak-finding algorithm."""
+"""A peak-finding algorithm."""
 
 
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
+    """Function that returns the peak in a list of unsorted integers."""
     if list_of_integers == []:
         return None
 
