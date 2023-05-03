fb3eb9b382bcc44a7e57f92861a89c74fdeeb951
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-05-03 14:47:17 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-# Read URL from command line argument
-url=$1
-
-# Send HTTP request using curl and store response body in a variable
-response_body=$(curl -sS $url)
-
-# Get size of response body in bytes and display it
-body_size=$(echo -n $response_body | wc -c)
-echo "Body size: $body_size bytes"
+# A bash Script that displays the Content-Length from a HTTP request
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
