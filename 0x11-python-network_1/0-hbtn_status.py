031d951d103e7eb1d8963d9abd5195888e76374c
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-05-04 12:21:04 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,10 +10,10 @@ def fetch_status():
 
     with urllib.request.urlopen(url) as response:
         body = response.read()
-     print("Body response:")
-     print("\t- type: {}".format(type(body)))
-     print("\t- content: {}".format(body))
-     print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
 
 
 
