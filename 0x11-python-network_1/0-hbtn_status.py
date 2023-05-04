8359e612e83a739166cc91c2797d86d22de6598e
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-05-04 12:27:22 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
-"""This script fetches the status of https://alx-intranet.hbtn.io using urllib"""
+"""This script gets the status of https://alx-intranet.hbtn.io using urllib"""
 
 import urllib.request
-
-
 def fetch_status():
-    """Fetches the status of https://alx-intranet.hbtn.io and displays the response body"""
+    """GETS the status of https://alx-intranet.hbtn.io & SHOWS the response body"""
     url = 'https://alx-intranet.hbtn.io/status'
-
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
-
-
-
 if __name__ == '__main__':
     fetch_status()
-
