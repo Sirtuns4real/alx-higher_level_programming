114b99376a05b8cb8d540ed343c6d51128341206
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-05-11 14:28:31 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get("http://0.0.0.0:5050/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
