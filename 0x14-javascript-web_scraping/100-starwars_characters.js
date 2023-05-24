455856491596c3835af029ee2fbc28b33164b2e3
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-05-24 03:56:14 +0100

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,18 +1,23 @@
-#!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.dev/api/people/' + process.argv[2];
-request(url, function (error, response, body) {
+
+const movieId = process.argv[2];
+
+const movieUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(movieUrl, function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    const characters = JSON.parse(body).results;
-    characters.forEach((character) => {
-      request(character.url, function (error, response, body) {
+    const movieData = JSON.parse(body);
+    const characterUrls = movieData.characters;
+
+    characterUrls.forEach((characterUrl) => {
+      request(characterUrl, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-          console.log(JSON.parse(body).name);
+          const characterData = JSON.parse(body);
+          console.log(characterData.name);
         }
       });
     });
   } else {
-    console.error('Error: ' + error);
+    console.error('Error requesting movie data:', error);
   }
 });
-
