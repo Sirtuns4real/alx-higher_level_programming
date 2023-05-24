3a8e3a320086115825604b9e324974fba68512a4
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-05-24 03:50:00 +0100

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/people/' + process.argv[2];
+const url = 'https://swapi.dev/api/people/' + process.argv[2];
 request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
+  if (!error && response.statusCode === 200) {
+    const characters = JSON.parse(body).results;
     characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
+      request(character.url, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
           console.log(JSON.parse(body).name);
         }
       });
     });
+  } else {
+    console.error('Error: ' + error);
   }
 });
+
