13dddf9b177645c1848bb0db49b2fcd30658ad3c
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-05-24 04:02:18 +0100

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,20 +1,4 @@
 #!/usr/bin/node
-
-const request = require('request');
 const fs = require('fs');
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8', function (err) {
-      if (err) {
-        console.error('Error writing file:', err);
-      } else {
-        console.log('File saved successfully.');
-      }
-    });
-  }
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
